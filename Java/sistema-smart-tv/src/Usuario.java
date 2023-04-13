65515c692638c8fc4b75ba49f6d182d5e1bf544d
Grocther <thiagocoghi@hotmail.com>
2023-04-13 01:26:17 -0300

    Aulas de estrutura de petição

---
+++ Java/sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,25 @@
+public class Usuario {
+
+    public static void main (String[] args) throws Exception{
+
+        SmartTv smartTv= new SmartTv();
+
+        smartTv.abaixarVolume();     
+        smartTv.abaixarVolume();
+        smartTv.abaixarVolume();  
+        smartTv.aumentarVolume();
+   
+
+        System.out.println("Tv Ligada?"+ smartTv.ligada );
+        System.out.println("Canal Atual : " + smartTv.canal );
+        System.out.println("Volume Atual : " + smartTv.volume );
+
+        smartTv.mudarCanal(15);
+        System.out.println("Canal Atual : " + smartTv.canal );
+        smartTv.ligar();
+        System.out.println("Novo status: Tv Ligada?"+ smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status: Tv Ligada?"+ smartTv.ligada);
+    }
+}
