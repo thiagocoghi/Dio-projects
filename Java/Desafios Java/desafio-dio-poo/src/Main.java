e4048efdd85229f61dc6f2ee187f97bd4a9fd836
Grocther <thiagocoghi@hotmail.com>
2023-04-29 14:57:00 -0300

    mentoria e curso

--- Java/Desafios Java/desafio-dio-poo/src/Main.java
+++ Java/Desafios Java/desafio-dio-poo/src/Main.java
@@ -1,5 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Aprendendo java");
+        curso.setCargaHoraria(8);
+
+        System.out.println(curso);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Ampliando suas habilidades em Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+
     }
+
+
 }
